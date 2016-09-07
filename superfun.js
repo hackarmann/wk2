dbb64512ca98f7eb3a89e46e0792ab523ed44e4f
hackarmann <anna.karmann92@gmail.com>
2016-09-07 13:18:04 +0200

    Create superfun.js
    Well, its ok i guess=)

---
+++ superfun.js
@@ -0,0 +1,14 @@
+var = num;
+var = doubleNum;
+num = 1000;
+function timesTwo (number) {
+return number * 2;
+};
+doubleNum = timesTwo(num);
+var = numbers;
+numbers = [num, doubleNum];
+for ( var i = 0; i < numbers.length; 1++);
+  timesTwo
+  
+numbers = {};
+numbers.num = doubleNum
